bd0ea8e7cd9feee0bd1b54aedc34306d16c47877
testgithub222222 <33162748+testgithub222222@users.noreply.github.com>
2017-10-30 23:56:07 -0600

    Update QueryBuilder.java

--- src/main/java/edu/csu2017fa314/DemoServer/Database/QueryBuilder.java
+++ src/main/java/edu/csu2017fa314/DemoServer/Database/QueryBuilder.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class QueryBuilder {
-    rivate String user = "";
+    private String user = "";
     private String pass = "";
 
     public QueryBuilder(String user, String pass) {
