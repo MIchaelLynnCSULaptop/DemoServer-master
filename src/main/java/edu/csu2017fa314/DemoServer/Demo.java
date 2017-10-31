83b8fdb1fc676f2cdd9d7cdf180dfbb9cc812367
testgithub222222 <michael.earl.lynn@gmail.com>
2017-10-30 21:56:18 -0600

    ok
    ok

--- src/main/java/edu/csu2017fa314/DemoServer/Demo.java
+++ src/main/java/edu/csu2017fa314/DemoServer/Demo.java
@@ -2,6 +2,7 @@ package edu.csu2017fa314.DemoServer;
 // master change
 import edu.csu2017fa314.DemoServer.Server.Server;
 //branch change
+//branch #2
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Welcome to TripCo");
